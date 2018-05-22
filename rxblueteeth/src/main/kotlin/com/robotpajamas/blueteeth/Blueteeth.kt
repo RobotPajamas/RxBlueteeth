187b71ce4826edf1aacf404d116c355fdcf36b8f
SJ <suresh@vicarasolutions.com>
2018-05-21 21:45:13 -0400

    First pass at reactive extensions for Blueteeth.
    Need to push latest Kotlin Blueteeth to Bintray, otherwise, can't really do this without lots of errors

---
+++ rxblueteeth/src/main/kotlin/com/robotpajamas/blueteeth/Blueteeth.kt
@@ -0,0 +1,58 @@
+package com.robotpajamas.blueteeth
+
+import io.reactivex.Observable
+import java.util.*
+
+class Reactive<Base>(val base: Base)
+
+val BlueteethDevice.rx: Reactive<BlueteethDevice>
+    get() = Reactive(this)
+
+fun <T> Reactive<T>.connect(): Observable<Boolean> where T : BlueteethDevice {
+    return Observable.create { emitter ->
+        this.base.connect { isConnected ->
+            emitter.onNext(isConnected)
+        }
+
+        emitter.setCancellable { /* TODO? */ }
+    }
+}
+
+fun <T> Reactive<T>.discoverServices(): Observable<Boolean> where T : BlueteethDevice {
+    return Observable.create { emitter ->
+        this.base.discoverServices { result ->
+            result.failure {
+                emitter.onError(result.error)
+            }
+            result.success {
+                emitter.onNext(discovered)
+                emitter.onComplete()
+            }
+        }
+
+        emitter.setCancellable { /* TODO? */ }
+    }
+}
+
+// Maybe this should be a Single/Completable vs Observable?
+fun <T> Reactive<T>.read(characteristic: UUID, service: UUID): Observable<Boolean> where T : BlueteethDevice {
+    return Observable.create { emitter ->
+        this.base.read(characteristic, service) { result ->
+            result.failure {
+                emitter.onError(it)
+            }
+            result.success {
+                emitter.onNext(it)
+                emitter.onComplete()
+            }
+        }
+
+        emitter.setCancellable { /* TODO? */ }
+    }
+}
+
+fun blahblah() {
+    val device = Blueteeth.peripherals[0]
+    device.rx.connect()
+            .subscribe()
+}
